205020e353176c6d130ba15a1799307fb311535d
Ivy Nguyen <ivynguyen003@gmail.com>
2018-05-16 14:28:02 -0700

    add link to firebase and snapshot

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js';
+
+var config = {
+  apiKey: "AIzaSyAIAFENMjKyeMklX30r5GvmC5PIK1RmDJM",
+  authDomain: "chat-app-ivy.firebaseapp.com",
+  databaseURL: "https://chat-app-ivy.firebaseio.com",
+  projectId: "chat-app-ivy",
+  storageBucket: "chat-app-ivy.appspot.com",
+  messagingSenderId: "814466748811"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
+  co
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <RoomList firebase={firebase}/>
+        </main>
       </div>
+
     );
   }
 }
