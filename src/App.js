fc4cebbb776ff02f4ca307a7210097c9ff297c75
Ivy Nguyen <ivynguyen003@gmail.com>
2018-05-30 10:54:57 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import * as firebase from 'firebase';
-import RoomList from './components/RoomList';
-import MessageList from './components/MessageList';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as firebase from "firebase";
+import RoomList from "./components/RoomList";
+import MessageList from "./components/MessageList";
 require("dotenv").config();
 
 var config = {
@@ -20,41 +20,36 @@ console.log(process.env.REACT_APP_FIREBASE);
 
 firebase.initializeApp(config);
 
-
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      activeRoom: '',
-      activeKey: '',
-    }
+      activeRoom: "",
+      activeKey: ""
+    };
   }
 
-  selectRoom=(room) => {
+  selectRoom = (room) => {
     this.setState({ activeKey: room.key });
     this.setState({ activeRoom: room });
     console.log(this);
-
-  }
+  };
 
   render() {
-    return (
-      <div className="App">
-        <main>
-          <aside>
-            <RoomList firebase={firebase}
-              activeRoom={this.selectRoom}
-            />
-          </aside>
-          <section>
+    return <div className="room-container">
+        <aside className="side-bar">
+          <h1>Let's Chat</h1>
+          <RoomList firebase={firebase} activeRoom={this.selectRoom} />
+        </aside>
+        <section className="room-display">
+          <section className="room-active">
             <h1>{this.state.activeRoom.name}</h1>
           </section>
-          <section>
+          <section className="message">
             <MessageList firebase={firebase} activeKey={this.state.activeKey} />
           </section>
-        </main>
-      </div>
-    );
+        </section>
+      </div>;
   }
 }
 
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,63 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import UserImage from "../profile.png";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faSearch from "@fortawesome/fontawesome-free-solid/faSearch";
+import "./RoomList.css";
+import "../App.css";
 
 class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       rooms: [],
-      name: ''
+      name: ""
     };
-    this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.roomsRef = this.props.firebase.database().ref("rooms");
   }
 
   componentDidMount() {
-    this.roomsRef.on('child_added', snapshot => {
+    this.roomsRef.on("child_added", snapshot => {
       const room = snapshot.val();
       room.key = snapshot.key;
-      this.setState({ rooms: this.state.rooms.concat(room), name:'' });
-    })
+      this.setState({ rooms: this.state.rooms.concat(room), name: "" });
+    });
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    if (!this.state.name) { return }
+    if (!this.state.name) {
+      return;
+    }
     this.roomsRef.push({ name: this.state.name });
-
   }
 
   handleChange(e) {
-    this.setState({ name: e.target.value })
+    this.setState({ name: e.target.value });
   }
 
-  activeRoom(room){
+  activeRoom(room) {
     this.props.activeRoom(room);
   }
 
   render() {
-    return (
-      <section>
-        <div >
-          {
-            this.state.rooms.map((room, index) =>
-              <ul className="each-room" key={index} > 
-                <li onClick = {()=>this.props.activeRoom(room)}>
-                {room.name} 
-                </li>
-              </ul>
-            )
-          }
+    return <section>
+        <div className="header">
+          <h2>Hello Ivy!</h2>
+          <img className="user" src={UserImage} alt={"user image"} />
         </div>
         <div>
-          <form onSubmit={(e) => this.handleSubmit(e)}>
-            <input name="text" value={this.state.name} onChange={(e) => this.handleChange(e)}>
-            </input>
-            <input type="submit" />
+          <form onSubmit={e => this.handleSubmit(e)}>
+            <input name="text" value={this.state.name} onChange={e => this.handleChange(e)} />
+            <button type="submit">
+              <FontAwesomeIcon icon={faSearch} pulse className="search" transform="grow-6" />
+            </button>
           </form>
         </div>
-      </section>
-    )
+        <section className="room-list">
+          {this.state.rooms.map((room, index) => (
+            <ul className="each-room" key={index}>
+              <li onClick={() => this.props.activeRoom(room)}>
+                {room.name}
+              </li>
+            </ul>
+          ))}
+        </section>
+      </section>;
   }
-
 }
 
 export default RoomList;
